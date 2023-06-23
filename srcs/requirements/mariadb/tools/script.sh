2d68ac2353fc7c8850ffe5234fed8a10f9b52220
Soufiane Nouae <snouae@e2r7p7.1337.ma>
2023-06-24 00:01:18 +0100

    first

---
+++ srcs/requirements/mariadb/tools/script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+service mysql start
+
+# Create the database
+DB_NAME="mydatabase"
+DB_USER="myuser"
+DB_PASS="mypassword"
+
+mysql  -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+
+# Create the user and grant privileges
+mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
+mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
+mysql -e "FLUSH PRIVILEGES;"
+mysql -e "alter user 'root'@'localhost' identified by '$DB_PASS';" ;
+mysql -e "FLUSH PRIVILEGES;"
+
+echo "MySQL database, user, and privileges have been set up successfully."
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+kill `cat /var/run/mysqld/mysqld.pid`
+# service -u root -p${DB_PASS} mysql stop
+
+#mysqladmin -u root -p"$DB_PASS" shutdown
+
+exec "$@"
