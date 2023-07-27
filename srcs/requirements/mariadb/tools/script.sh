c147eb2f602e7db14e15b804178763f26207c7a7
Soufiane Nouae <snouae@e3r6p7.1337.ma>
2023-07-27 15:42:33 +0100

    to linux

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 # Create the database
-DB_NAME="mydatabase"
-DB_USER="myuser"
-DB_PASS="mypassword"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+
 
 service mysql start
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+#mysqld_safe
+
 
 mysql  -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 
 # Create the user and grant privileges
+
 mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
 mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
-mysql -e "FLUSH PRIVILEGES;"
 mysql -e "alter user 'root'@'localhost' identified by '$DB_PASS';" ;
-mysql -e "FLUSH PRIVILEGES;"
 
 # echo "MySQL database, user, and privileges have been set up successfully."
 
 kill `cat /var/run/mysqld/mysqld.pid`
-# service -u root -p${DB_PASS} mysql stop
-
+ #service -u root -p${DB_PASS} mysql stop
 #mysqladmin -u root -p"$DB_PASS" shutdown
 
-exec "$@" 
+#exec "$@"
+
+mysqld_safe
