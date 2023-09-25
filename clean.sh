d2f19e025c2cb5cb28a5effeb59f6154953cbcdd
snouae <soufianenouae01@gmail.com>
2023-09-25 19:31:05 +0100

    4

--- clean.sh
+++ clean.sh
@@ -5,3 +5,5 @@ docker volume rm $(docker volume ls -q)
 docker network rm $(docker network ls -q) 2>/dev/null
 docker image rm $(docker image ls -q)
         
+sudo rm -rf /home/snouae/Desktop/data/mariadb/*
+sudo rm -rf /home/snouae/Desktop/data/wordpress/*
